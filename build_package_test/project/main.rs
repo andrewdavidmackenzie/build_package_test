68e07d833101af5ec844c0c1d8b2c9b9f5175a21
Andrew Mackenzie <andrew@mackenzie-serres.net>
2022-01-23 16:35:50 +0100

    Make as similar to my project as possible - still working

--- build_package_test/project/main.rs
+++ build_package_test/project/main.rs
@@ -4,7 +4,7 @@ pub fn main() -> io::Result<()>{
     println!("build_package_test binary");
     println!("List of modules loaded via generated.rs file");
 
-    build_package_test::list_modules();
+    project::list_modules();
 
     Ok(())
 }
